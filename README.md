94236f3af93f683b911c49a832b8432a934cfb03
jordsan <mail.jordan.michaels@gmail.com>
2017-12-12 07:22:11 +0000

    tweets work from text

---
+++ README.md
@@ -0,0 +1,14 @@
+
+# Markov Tweets
+
+## By Jordan Michaels
+
+CPEG476 Project 3 - Markovian Tweets
+
+How to run the project: 
+
+g++ markov.cpp -o markov.out -std=c++11
+
+then
+
+./markov.out
---
+++ markov.cpp
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> 
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+#include <algorithm> 
+using namespace std;
+
+void removeCharsFromString(string &str, const char* charsToRemove) {
+    for (int i = 0; i < strlen(charsToRemove); i++) {
+        str.erase(remove(str.begin(), str.end(), charsToRemove[i]), str.end());
+    }
+}
+
+map<string, map<string, int> > readFile(string input) {
+    ifstream file;
+    file.open(input.c_str());
+    
+    map<string, map<string, int> > output;
+    map<string, map<string, int> > ::iterator it;
+    map<string, int> ::iterator it2;
+
+    string currWord = "";
+    string prevWord = "";
+    string pattern = "().$*%'`&#@=+{}[]|\\;:><~ ";
+    while (file >> currWord) {
+        bool check = false;
+        
+        removeCharsFromString(currWord, pattern.c_str());
+        
+        if (prevWord.compare("")) {
+            it = output.find(prevWord);
+            if (it != output.end()) {
+                it2 = output[prevWord].find(currWord);
+                if (it != output.end()) {
+                   output[prevWord][currWord] ++; 
+                } 
+                else {
+                    output.at(prevWord).insert(pair<string, int>(currWord, 1));
+                }
+            }
+            else {
+                output[prevWord] = {{currWord, 1}};
+            }
+        }
+        
+        prevWord = currWord;
+    }
+    
+    return output;
+}
+
+vector<string> generateTweets(map<string, map<string, int> > input){
+    vector<string> output;
+    vector<pair<string, pair<string, int> > > pairList;
+    string tweet = "";
+    
+    for (auto& x: input) {
+        for (auto& y: x.second) {
+            pairList.push_back(make_pair(x.first, make_pair(y.first, y.second)));
+        }
+    }
+    
+    sort(pairList.begin(), pairList.end(), [=](pair<string, pair<string, int> > a, pair<string, pair<string, int> > b){
+        return a.second.second > b.second.second;
+    });
+    
+    for (auto it = pairList.begin(); it != pairList.end(); it++ ){
+        if (tweet.length() + it->first.length() + it->second.first.length() + 2 <= 280){
+            tweet += it->first + " " + it->second.first + " ";
+        }
+        else {
+            output.push_back(tweet);
+            tweet = "";
+        }
+    }
+    
+    return output;
+}
+
+int main () {
+    map<string, map<string, int> > pairMap;
+    pairMap = readFile("input.txt");
+    
+    int number = 0;
+    vector<string> tweets = generateTweets(pairMap);
+    
+    cout << "Enter desired number of tweets : ";
+    cin >> number;
+    
+    while (number > tweets.size()){
+        cout << "Error: cannot generate " << number << " tweets with given text" << endl;
+        cout << "Enter desired number of tweets : ";
+        cin >> number;
+    }
+    
+    cout << endl;
+    for (int i = 0; i < number; i++) {
+        cout << "Tweet " << i+1 << " : " << endl << endl;
+        cout << tweets.at(i) << endl << endl;
+    }
+    
+    return 0;
+}
+
